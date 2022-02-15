01af3388ac6413b2a34b240fe8355880e18b8732
Jon Spindler <jonkspindler@gmail.com>
2022-02-15 14:58:38 -0500

    Created File
    Notes on solidity loops

---
+++ MyLoop.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+/// @author Jon K. Spindler
+/// @notice My notes on solidity Loops
+
+pragma solidity ^0.8.0;
+
+contract MyLoop {
+    
+    function myLoop() public pure {
+        for(uint256 i = 0; i < 5; i++) {
+            if(i == 3){
+                break;
+            }
+            if(i == 2) {
+                continue;
+            }
+        }
+        uint256 j = 0;
+        while(j < 10) {
+        // do any actions
+            j++;
+    }
+    }
+}
