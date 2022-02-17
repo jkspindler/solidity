530d184a92c40d8cd6c99c77dcc1c908843c6f83
Jon Spindler <jonkspindler@gmail.com>
2022-02-17 09:03:26 -0600

    Created File
    Notes on solidity events

---
+++ MyEvents.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/** 
+ * @author Jon K. Spindler
+ * @dev Notes on solidity midifiers
+ */
+contract MyEvents {
+
+    event CreatedNFT(address indexed user, uint256 id, uint dna);
+
+    function createNft(uint256 _id, uint256 _dna) public {
+        // create the NFT and save to the storage
+        emit CreatedNFT(msg.sender, _id, _dna);
+    }
+}
