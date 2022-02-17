06b7d89f83173884660f013018fbe8322963551e
Jon Spindler <jonkspindler@gmail.com>
2022-02-17 09:51:18 -0600

    Created File
    Notes on interfaces in solidity

---
+++ MyInterface.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/** 
+ * @author Jon K. Spindler
+ * @dev Notes on solidity interfaces
+ */
+
+interface ICounter {
+    function count() external view returns (uint256);
+    function addToCount() external;
+}
+
+contract MyInterface is ICounter {
+    uint256 counter = 0;
+
+    function count() external view override returns (uint256) {
+        return counter;
+    }
+
+    function addToCount() external override {
+        counter++;
+    }
+
+}
